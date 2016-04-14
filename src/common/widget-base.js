e852fadc787e46f7dc95b91513c993ef1b0a1e90
Mydeen-SN <mydeensn@syncfusion.com>
2016-04-14 18:51:31 +0530

    Events,widget-base,wiget-base spec
    Added JsDoc for widget-base and events and completed the test case for
    widgetBase class

--- src/common/widget-base.js
+++ src/common/widget-base.js
@@ -2,11 +2,19 @@ import {getEventOption} from './events';
 import {TaskQueue} from 'aurelia-task-queue';
 export class WidgetBase {
 
+/**
+* To Create an widget
+* @param option Object which contains  Element in which  widget will be created
+*/
   createWidget(option) {
     let allOption = this.getWidgetOptions(option.element);
     this.widget = jQuery($(option.element))[this.controlName](allOption).data(this.controlName);
   }
 
+/**
+* To get property and event options from the element
+* @param element Element from which options are acquired
+*/
   getWidgetOptions(element) {
     let propOptions = this.util.getOptions(this);
     let eventOption = getEventOption(element);
@@ -16,6 +24,12 @@ export class WidgetBase {
   attached() {
     this.createWidget({ element: this.element });
   }
+/**
+ * To change widget model value
+ * @param property The viewModel property name
+ * @param newValue New value of the property
+ * @param oldvalue Pld value of the property
+ */
   propertyChanged(property, newValue, oldValue) {
     if(this.widget) {
       let prop = this.util.getControlPropertyName(this, property);
